4a9025731d44ad727c438dd963ca8e91b0d0b06c
Victor Rodrigues <victor-dev@outlook.com>
2023-10-29 17:09:26 -0300

    atualizando portifolio

--- src/components/ProjectsSection/ProjectMocksFront.jsx
+++ src/components/ProjectsSection/ProjectMocksFront.jsx
@@ -1,24 +1,27 @@
-import encontreAmeixa from "../../assets/img/projects/encontreameixa.png"
+import encontreAmeixa from "../../assets/img/projects/encontreameixa.png";
 import projImg1 from "../../assets/img/projects/project-img1.png";
 import projImg2 from "../../assets/img/projects/project-img2.png";
 import projImg3 from "../../assets/img/projects/project-img3.png";
+import mpeWeekSeller from "../../assets/img/projects/mpeweekSeller.png";
+import mpeWeekMarketPlace from "../../assets/img/projects/mpeweekMarketPlace.png";
 
 export const ProjectMocksFront = [
   {
     title: "Encontre Ameixa",
-    description: "O site foi desenvolvido em React e tem como objetivo ajudar os clientes a encontrar fornecedores próximos a eles. Para obter os dados de localização, utilizei a API Geocoder do Google Cloud Platform",
+    description:
+      "O site foi desenvolvido em React e tem como objetivo ajudar os clientes a encontrar fornecedores próximos a eles. Para obter os dados de localização, utilizei a API do GoogleMaps",
     imgUrl: encontreAmeixa,
-    link: "https://encontreameixa.com.br"
+    link: "https://encontreameixa.com.br",
   },
   {
-    title: "Business Startup",
-    description: "Design e Development",
-    imgUrl: projImg2,
+    title: "MPE Week Ambiente do Seller",
+    description: "Participei do desenvolvimento dessa plataforma em uma equipe composta por 3 programadores onde o objetivo era criar um ambiente onde os sellers poderiam cadatrar seus produtos e fazer a gestão da sua loja online",
+    imgUrl: mpeWeekSeller,
   },
   {
-    title: "Business Startup",
-    description: "Design e Development",
-    imgUrl: projImg3,
+    title: "MPE Week Marktplace",
+    description: "Participei do desenvolvimento desse marketplace em uma equipe composta por 3 programadores onde o objetivo era criar um marketplace onde os usuários poderiam regastar cupons e consumir produtos com desconto localmente",
+    imgUrl: mpeWeekMarketPlace,
   },
   {
     title: "Business Startup",
